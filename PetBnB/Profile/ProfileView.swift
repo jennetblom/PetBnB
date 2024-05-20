4ed648ee01d458fadbf5d6084dd76aacb9fb8690
jennetblom <jeboblom20@hotmail.com>
2024-05-20 15:41:30 +0200

    added segmentedPicker

--- PetBnB/Profile/ProfileView.swift
+++ PetBnB/Profile/ProfileView.swift
@@ -1,10 +1,50 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedSegment = 0
+    let segments = ["Uthyrare", "Hyresgäst"]
     var body: some View {
-        Text("Profil")
+        VStack{
+            Picker("Select View", selection: $selectedSegment) {
+                         ForEach(0..<segments.count) { index in
+                             Text(segments[index])
+                                 .tag(index)
+                         }
+                     }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            if selectedSegment == 0 {
+                HomeOwnerView()
+            } else {
+                HomeGuestView()
+            }
+            Spacer()
+        }
     }
 }
+struct HomeOwnerView : View {
+    var body: some View {
+        VStack{
+            Image(systemName: "cat")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding()
+//            Text("Boende")
+//                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                .padding(.trailing, 250)
+//            Text("Djur")
+//                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                .padding(.trailing, 250)
+        }
+    }
+}
+struct HomeGuestView : View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
 
 #Preview {
     ProfileView()
