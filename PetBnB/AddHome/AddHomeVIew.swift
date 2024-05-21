77eb42ef7b78270a0409ad44e0230c4f7ba4522b
Ina Burström <inaburstrom@hotmail.com>
2024-05-21 13:26:44 +0200

    first sections added in add home view.

--- PetBnB/AddHome/AddHomeVIew.swift
+++ PetBnB/AddHome/AddHomeVIew.swift
@@ -1,2 +1,31 @@
 
-import Foundation
+import SwiftUI
+
+struct AddHomeVIew: View {
+    @State private var beds: String = "6"
+    
+    var body: some View {
+        VStack{
+            Form {
+                Section(header: Text("Boende")) {
+                    HStack{
+                        Text("Antal bäddar:")
+                        TextField("Antal sängar", text: $beds) }
+                }
+                
+                Section(header: Text("Djur")) {
+                    
+                }
+                
+            }
+            
+            .onAppear{
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    AddHomeVIew()
+}
