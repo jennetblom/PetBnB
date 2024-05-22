ca085185da96b148591cfdca03f14cbb4ddf78c4
jennetblom <jeboblom20@hotmail.com>
2024-05-22 16:22:28 +0200

    added profilelayout for guest

--- PetBnB/Profile/ProfileVIewModel.swift
+++ PetBnB/Profile/ProfileVIewModel.swift
@@ -1,3 +1,11 @@
 
 import Foundation
+import Firebase
 
+class ProfileViewModel: ObservableObject {
+    
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+    
+    @Published var users = [User]()
+}
--- PetBnB/Profile/ProfileView.swift
+++ PetBnB/Profile/ProfileView.swift
@@ -5,6 +5,7 @@ struct ProfileView: View {
     @State var rating = 4
     
     let segments = ["Uthyrare", "Hyresgäst"]
+    
     var body: some View {
         VStack{
             Picker("Select View", selection: $selectedSegment) {
@@ -16,7 +17,10 @@ struct ProfileView: View {
             .pickerStyle(SegmentedPickerStyle())
             .padding()
             
+            profileHeaderWithImageAndStars(rating: $rating)
+            
             if selectedSegment == 0 {
+              
                 HomeOwnerView()
             } else {
                 HomeGuestView()
@@ -26,23 +30,9 @@ struct ProfileView: View {
     }
 }
 struct HomeOwnerView : View {
-    @State var rating = 3
     var body: some View {
             VStack{
                 ScrollView {
-                    HStack{
-                        Image("catimage")
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                            .offset(x: 20)
-                        VStack{
-                            Text("Martin")
-                                .font(.title2)
-                                .offset(x:-20, y: 0)
-                            RatingBar(rating: $rating)
-                                .offset(x: 25, y: -5)
-                        }
-                    }.padding()
                     HousingInfoView()
                     AnimalInfoView()
                 }
@@ -54,6 +44,24 @@ struct HomeOwnerView : View {
                     .cornerRadius(10.0)
                     .padding()
             }
+    }
+}
+struct profileHeaderWithImageAndStars : View {
+    @Binding var rating : Int
+    var body: some View {
+        HStack{
+            Image("catimage")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .offset(x: 0)
+            VStack{
+                Text("Martin")
+                    .font(.title2)
+                    .offset(x:-35, y: 0)
+                RatingBar(rating: $rating)
+                    .offset(x: 10, y: -5)
+            }
+        }.padding()
     }
 }
 struct HousingInfoView : View {
@@ -114,7 +122,7 @@ struct HousingInfoView : View {
                             .frame(height: 70)
                             .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
                             .multilineTextAlignment(.leading)
-                            .offset(y: -30)
+                            .offset(x: 8, y: -30)
 
                 }.padding(.horizontal, 10)
                
@@ -137,8 +145,7 @@ struct AnimalInfoView : View {
                     .fontWeight(.bold)
                 Spacer()
             }
-            
-            RowView(title: "Typ") {
+            RowView(title: "Typ av djur") {
                 Picker("Välj djur", selection: $selectedAnimal){
                     ForEach(animals, id: \.self) { animal in
                         Text(animal).tag(animal)
@@ -164,7 +171,7 @@ struct AnimalInfoView : View {
                         .frame(height: 70)
                         .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
                         .multilineTextAlignment(.leading)
-                        .offset(y: -30)
+                        .offset(x: 8, y: -30)
 
             }.padding(.horizontal, 10)
         }
@@ -217,37 +224,96 @@ struct InfoRowView<Content : View>:View {
 
 struct HomeGuestView : View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
-    }
-}
-struct StarView : View {
-    var filled : Bool
-    var size : CGFloat = 20
-    
-    var body: some View {
-        Image(systemName: filled ? "star.fill" : "star")
-            .resizable()
-            .frame(width: size, height: size)
-            .foregroundColor(filled ? .yellow : .gray)
-    }
-}
-struct RatingBar: View {
-    @Binding var rating: Int
-    var maximumRating: Int = 5
-    var starSize: CGFloat = 24
-    
-    var body: some View {
-        HStack {
-            ForEach(1..<maximumRating + 1, id: \.self) { number in
-                StarView(filled: number <= rating, size: starSize)
-                    .onTapGesture {
-                        rating = number
-                    }
+        VStack{
+            ScrollView {
+                AboutMeGuestView()
+                    .padding(.vertical)
+                GuestAnimalExperienceView()
             }
+            Button("Spara") {
+                
+            }.frame(width: 220, height: 40)
+                .background(Color("primary"))
+                .foregroundColor(.black)
+                .cornerRadius(10.0)
+                .padding()
         }
     }
 }
+struct AboutMeGuestView : View {
+    
+    var ages = Array(1...100)
+    
+    @State var selectedAge = 9
+    @State var infoGuest = ""
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Om mig")
+                    .fontWeight(.bold)
+                    .padding(.horizontal)
+                Spacer()
+            }
+            
+            RowView(title: "Ålder") {
+                Picker("Välj ålder", selection: $selectedAge){
+                    ForEach(ages, id: \.self) { age in
+                        Text("\(age)").tag(age)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+                .accentColor(.gray)
+            }.padding(.horizontal, 10)
+            
+            InfoRowView(title: "Berätta lite om dig själv") {
+                    TextEditor(text: $infoGuest)
+                        .scrollContentBackground(.hidden)
+                        .frame(height: 70)
+                        .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
+                        .multilineTextAlignment(.leading)
+                        .offset(x: 8, y: -30)
 
+            }.padding(.horizontal, 10)
+        }
+    }
+}
+struct GuestAnimalExperienceView : View {
+    
+    @State var selectedAnimal = "Katt"
+    @State var animalExperienceInfoText = ""
+    var animals = ["Hund", "Katt", "Fågel", "Fisk", "Reptil", "Pälsdjur"]
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Djurerfarenhet")
+                    .fontWeight(.bold)
+                    .padding(.horizontal)
+                Spacer()
+            }
+            RowView(title: "Typ av djur") {
+                Picker("Välj djur", selection: $selectedAnimal){
+                    ForEach(animals, id: \.self) { animal in
+                        Text(animal).tag(animal)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+                .accentColor(.gray)
+            }.padding(.horizontal, 10)
+            
+            InfoRowView(title: "Vad har du för erfarenhet av djur?") {
+                TextEditor(text: $animalExperienceInfoText)
+                    .scrollContentBackground(.hidden)
+                    .frame(height: 70)
+                    .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
+                    .multilineTextAlignment(.leading)
+                    .offset(x: 8, y: -30)
+            }.padding(.horizontal, 10)
+        }
+    }
+    
+}
 
 #Preview {
     ProfileView()
