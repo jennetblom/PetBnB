b3afb04a1633a14d4e5cef9110aa14aa17a0eeaf
Ina Burström <inaburstrom@hotmail.com>
2024-05-22 13:05:57 +0200

    Updated imagepicker to adapt to choosing multiple pics.

--- PetBnB/AddHome/ImagePicker.swift
+++ PetBnB/AddHome/ImagePicker.swift
@@ -5,4 +5,61 @@
 //  Created by Ina Burström on 2024-05-21.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImages: [UIImage]
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 0 // 0 betyder ingen gräns
+
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+
+            let group = DispatchGroup()
+
+            for result in results {
+                group.enter()
+                let provider = result.itemProvider
+
+                if provider.canLoadObject(ofClass: UIImage.self) {
+                    provider.loadObject(ofClass: UIImage.self) { (image, error) in
+                        DispatchQueue.main.async {
+                            if let uiImage = image as? UIImage {
+                                self.parent.selectedImages.append(uiImage)
+                            }
+                            group.leave()
+                        }
+                    }
+                } else {
+                    group.leave()
+                }
+            }
+
+            group.notify(queue: .main) {
+                // Handling after all images are loaded
+            }
+        }
+    }
+}
