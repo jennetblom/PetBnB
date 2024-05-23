8dc46ce17360cfdd0309372e1fa5b2103cb48d16
Gurkansson <fil.gustavsson@gmail.com>
2024-05-23 09:19:09 +0200

    frida

--- PetBnB/LogIn/LogInView.swift
+++ PetBnB/LogIn/LogInView.swift
@@ -5,25 +5,42 @@ import Firebase
 struct LogInView: View {
     @Binding var signedIn: Bool
     var auth = Auth.auth()
+    @State private var showSignUpView = false
     
     var body: some View {
-        VStack {
-            Button("Logga in") {
-                auth.signInAnonymously { result, error in
-                    if error != nil {
-                        print("error signing in")
-                    } else {
-                        signedIn = true
+            VStack {
+                Button("Logga in") {
+                    auth.signInAnonymously { result, error in
+                        if error != nil {
+                            print("error signing in")
+                        } else {
+                            signedIn = true
+                        }
                     }
                 }
+                .padding()
+                .background(Color("primary"))
+                .foregroundColor(Color("text"))
+                .cornerRadius(8)
+                
+                Button(action: {
+                    self.showSignUpView = true
+                }) {
+                    Text("Registrera")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+                .sheet(isPresented: $showSignUpView) {
+                    SignUpView()
             }
-            .padding()
-            .background(Color("primary"))
-            .foregroundColor(Color("text"))
-            .cornerRadius(8)
+            
         }
     }
 }
+
 
 #Preview {
     LogInView(signedIn: .constant(false))
