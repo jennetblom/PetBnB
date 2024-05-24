a37b11f3999941976a5d322d8824247e257c2f75
jennetblom <jeboblom20@hotmail.com>
2024-05-24 12:58:03 +0200

    animalStructure-jennet

--- PetBnB/Profile/HomeOwnerView.swift
+++ PetBnB/Profile/HomeOwnerView.swift
@@ -15,17 +15,28 @@ struct HomeOwnerView : View {
         VStack{
             ScrollView {
                 HousingInfoView()
-                AnimalInfoView()
+                
+                animalInfoList
+                
             }
-            //                Button("Spara") {
-            //                    
-            //                }.frame(width: 220, height: 40)
-            //                    .background(Color("primary"))
-            //                    .foregroundColor(.black)
-            //                    .cornerRadius(10.0)
-            //                    .padding()
-            //            }
+            
         }
+    }
+    
+    private var animalInfoList: some View {
+        ForEach(0..<viewModel.animalCount, id: \.self) { index in
+            AnimalInfoViewWrapper(index: index)
+        }
+    }
+    private func AnimalInfoViewWrapper(index: Int) -> some View {
+        AnimalInfoView(
+            index: index,
+            animalType: $viewModel.animalType[index],
+            animalAge: $viewModel.animalAge[index],
+            animalInfo: $viewModel.animalInfo[index], // Assuming this was a typo and should be animalInfo
+            isLast: index == viewModel.animalCount - 1,
+            addAnimalAction: viewModel.addAnimal
+        )
     }
 }
 struct HousingInfoView : View {
@@ -36,63 +47,69 @@ struct HousingInfoView : View {
     var rooms = Array(0...10)
     
     var body: some View {
-            VStack{
-                HStack{
-                    Text("Boende")
-                        .padding(.horizontal)
-                        .fontWeight(.bold)
-                    Spacer()
+        VStack{
+            HStack{
+                Text("Boende")
+                    .padding(.horizontal)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            RowView(title: "Stad") {
+                Picker("Välj stad", selection: $viewModel.city){
+                    ForEach(cities, id: \.self) { city in
+                        Text(city).tag(city)
+                    }
                 }
-                
-                RowView(title: "Stad") {
-                    Picker("Välj stad", selection: $viewModel.city){
-                        ForEach(cities, id: \.self) { city in
-                            Text(city).tag(city)
-                        }
+                .pickerStyle(MenuPickerStyle())
+                .accentColor(.gray)
+            }.padding(.horizontal, 10)
+            
+            
+            RowView(title: "Sovplatser") {
+                Picker("Välj sovplatser", selection: $viewModel.numberOfBeds) {
+                    ForEach(beds, id: \.self) { bed in
+                        Text("\(bed)").tag(bed)
                     }
-                    .pickerStyle(MenuPickerStyle())
-                    .accentColor(.gray)
-                }.padding(.horizontal, 10)
-                
-                
-                RowView(title: "Sovplatser") {
-                    Picker("Välj sovplatser", selection: $viewModel.numberOfBeds) {
-                        ForEach(beds, id: \.self) { bed in
-                            Text("\(bed)").tag(bed)
-                        }
+                }
+                .pickerStyle(MenuPickerStyle())
+                .accentColor(.gray)
+            }.padding(.horizontal,10)
+            
+            RowView(title: "Antal rum") {
+                Picker("Välj antal rum", selection: $viewModel.numberOfRooms) {
+                    ForEach(rooms, id: \.self) { room in
+                        Text("\(room)").tag(room)
                     }
-                    .pickerStyle(MenuPickerStyle())
-                    .accentColor(.gray)
-                }.padding(.horizontal,10)
+                }
+                .pickerStyle(MenuPickerStyle())
+                .accentColor(.gray)
+            }.padding(.horizontal,10)
+            
+            InfoRowView(title: "Info om boendet") {
+                TextEditor(text: $viewModel.homeInfo)
+                    .scrollContentBackground(.hidden)
+                    .frame(height: 70)
+                    .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
+                    .multilineTextAlignment(.leading)
+                    .offset(x: 8, y: -30)
                 
-                RowView(title: "Antal rum") {
-                    Picker("Välj antal rum", selection: $viewModel.numberOfRooms) {
-                        ForEach(rooms, id: \.self) { room in
-                            Text("\(room)").tag(room)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .accentColor(.gray)
-                }.padding(.horizontal,10)
-                
-                InfoRowView(title: "Info om boendet") {
-                    TextEditor(text: $viewModel.homeInfo)
-                            .scrollContentBackground(.hidden)
-                            .frame(height: 70)
-                            .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
-                            .multilineTextAlignment(.leading)
-                            .offset(x: 8, y: -30)
-
-                }.padding(.horizontal, 10)
-               
-            }.padding(.vertical)
-        }
+            }.padding(.horizontal, 10)
+            
+        }.padding(.vertical)
+    }
 }
 struct AnimalInfoView : View {
     
     @EnvironmentObject var viewModel: ProfileViewModel
     var animals = ["Hund", "Katt", "Fågel", "Fisk", "Reptil", "Pälsdjur"]
     var ages = Array(0...100)
+    var index : Int
+    @Binding var animalType: String
+    @Binding var animalAge: Int
+    @Binding var animalInfo: String
+    var isLast : Bool
+    var addAnimalAction : () -> Void
     
     var body: some View {
         VStack{
@@ -103,7 +120,7 @@ struct AnimalInfoView : View {
                 Spacer()
             }
             RowView(title: "Typ av djur") {
-                Picker("Välj djur", selection: $viewModel.animalType){
+                Picker("Välj djur", selection: $animalType){
                     ForEach(animals, id: \.self) { animal in
                         Text(animal).tag(animal)
                     }
@@ -113,7 +130,7 @@ struct AnimalInfoView : View {
             }.padding(.horizontal, 10)
             
             RowView(title: "Ålder") {
-                Picker("Välj ålder", selection: $viewModel.animalAge){
+                Picker("Välj ålder", selection: $animalAge){
                     ForEach(ages, id: \.self) { age in
                         Text("\(age)").tag(age)
                     }
@@ -123,14 +140,24 @@ struct AnimalInfoView : View {
             }.padding(.horizontal, 10)
             
             InfoRowView(title: "Info om djur") {
-                TextEditor(text: $viewModel.animalInfo)
-                        .scrollContentBackground(.hidden)
-                        .frame(height: 70)
-                        .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
-                        .multilineTextAlignment(.leading)
-                        .offset(x: 8, y: -30)
-
+                TextEditor(text: $animalInfo)
+                    .scrollContentBackground(.hidden)
+                    .frame(height: 70)
+                    .padding(EdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2))
+                    .multilineTextAlignment(.leading)
+                    .offset(x: 8, y: -30)
+                
             }.padding(.horizontal, 10)
+            
+            if isLast {
+                Button(action: addAnimalAction) {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Lägg till djur")
+                            .foregroundStyle(Color("secondary"))
+                    }
+                }
+            }
         }
     }
 }
--- PetBnB/Profile/ProfileVIewModel.swift
+++ PetBnB/Profile/ProfileVIewModel.swift
@@ -15,64 +15,89 @@ class ProfileViewModel: ObservableObject {
     @Published var numberOfBeds = 0
     @Published var numberOfRooms = 0
     @Published var homeInfo = ""
-    @Published var animalType = "Katt"
-    @Published var animalAge = 0
-    @Published var animalInfo = ""
+    @Published var animalCount = 1
+    @Published var animalType = ["Katt"]
+    @Published var animalAge = [0]
+    @Published var animalInfo = [""]
     @Published var userAge = 0
     @Published var userInfo = ""
     @Published var animalExperienceType = "Reptil"
     @Published var animalExperienceInfo = ""
     
+    func addAnimal() {
+         animalType.append("")
+         animalAge.append(0)
+         animalInfo.append("")
+         animalCount += 1
+     }
     func fetchUserProfileFromFirebase() {
-            guard let user = auth.currentUser else { return }
-            
-            db.collection("users").document(user.uid).getDocument { document, error in
-                if let document = document, document.exists {
-                    let data = document.data()
-                    self.name = data?["name"] as? String ?? ""
-                    self.rating = data?["rating"] as? Int ?? 0
-                    self.city = data?["city"] as? String ?? "Göteborg"
-                    self.numberOfBeds = data?["numberOfBeds"] as? Int ?? 0
-                    self.numberOfRooms = data?["numberOfRooms"] as? Int ?? 0
-                    self.homeInfo = data?["homeInfo"] as? String ?? ""
-                    self.animalType = data?["animalType"] as? String ?? "Reptil"
-                    self.animalAge = data?["animalAge"] as? Int ?? 0
-                    self.animalInfo = data?["animalInfo"] as? String ?? ""
-                    self.userAge = data?["userAge"] as? Int ?? 0
-                    self.userInfo = data?["userInfo"] as? String ?? ""
-                    self.animalExperienceType = data?["animalExperienceType"] as? String ?? "Katt"
-                    self.animalExperienceInfo = data?["animalExperienceInfo"] as? String ?? ""
-                } else {
-                    print("Document does not exist")
+        guard let user = auth.currentUser else { return }
+        
+        db.collection("users").document(user.uid).getDocument { [weak self] document, error in
+            guard let self = self else { return }
+            if let document = document, document.exists {
+                guard let data = document.data() else { return }
+                DispatchQueue.main.async {
+                    self.name = data["name"] as? String ?? ""
+                    self.rating = data["rating"] as? Int ?? 0
+                    self.city = data["city"] as? String ?? "Göteborg"
+                    self.numberOfBeds = data["numberOfBeds"] as? Int ?? 0
+                    self.numberOfRooms = data["numberOfRooms"] as? Int ?? 0
+                    self.homeInfo = data["homeInfo"] as? String ?? ""
+                    self.userAge = data["userAge"] as? Int ?? 0
+                    self.userInfo = data["userInfo"] as? String ?? ""
+                    self.animalExperienceType = data["animalExperienceType"] as? String ?? "Katt"
+                    self.animalExperienceInfo = data["animalExperienceInfo"] as? String ?? ""
+                    
+                    if let animalsData = data["animals"] as? [String: [String: Any]] {
+                        self.animalType.removeAll()
+                        self.animalAge.removeAll()
+                        self.animalInfo.removeAll()
+                        
+                        for index in 0..<animalsData.count {
+                            if let animalData = animalsData["animal\(index)"] {
+                                self.animalType.append(animalData["type"] as? String ?? "Reptil")
+                                self.animalAge.append(animalData["age"] as? Int ?? 0)
+                                self.animalInfo.append(animalData["additionalInfoAnimal"] as? String ?? "")
+                            }
+                        }
+                        self.animalCount = animalsData.count
+                    }
                 }
+            } else {
+                print("Document does not exist")
             }
         }
-        func saveUserProfileToFirebase() {
-            
-            guard let user = auth.currentUser else {return}
-            
-            let userProfileData: [String: Any] = [
-                "rating": self.rating,
-                "city": self.city,
-                "numberOfBeds": self.numberOfBeds,
-                "numberOfRooms": self.numberOfRooms,
-                "homeInfo": self.homeInfo,
-                "animalType": self.animalType,
-                "animalAge": self.animalAge,
-                "animalInfo": self.animalInfo,
-                "userAge": self.userAge,
-                "userInfo": self.userInfo,
-                "animalGuestExperienceType": self.animalExperienceType,
-                "animalGuestExperienceInfo": self.animalExperienceInfo
-            ]
-            
-            db.collection("users").document(user.uid).setData(userProfileData, merge: true) { error in
-                if let error = error {
-                    print("Error writing document: \(error)")
-                } else {
-                    print("Document sucessfully written!")
-                }
-                
-            }
     }
+        func saveUserProfileToFirebase() {
+            guard let user = auth.currentUser else { return }
+                 
+                 var animalInfos: [String: AnimalInfo] = [:]
+                 for index in 0..<animalCount {
+                     let animalInfo = AnimalInfo(type: animalType[index], age: animalAge[index], additionalInfoAnimal: animalInfo[index])
+                     animalInfos["animal\(index)"] = animalInfo
+                 }
+                 
+                 let userProfileData: [String: Any] = [
+                     "name": self.name,
+                     "rating": self.rating,
+                     "city": self.city,
+                     "numberOfBeds": self.numberOfBeds,
+                     "numberOfRooms": self.numberOfRooms,
+                     "homeInfo": self.homeInfo,
+                     "animals": animalInfos.mapValues { ["type": $0.type, "age": $0.age, "additionalInfoAnimal": $0.additionalInfoAnimal] },
+                     "userAge": self.userAge,
+                     "userInfo": self.userInfo,
+                     "animalExperienceType": self.animalExperienceType,
+                     "animalExperienceInfo": self.animalExperienceInfo
+                 ]
+                 
+                 db.collection("users").document(user.uid).setData(userProfileData, merge: true) { error in
+                     if let error = error {
+                         print("Error writing document: \(error)")
+                     } else {
+                         print("Document successfully written!")
+                     }
+                 }
+             }
 }
