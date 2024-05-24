da22173bd0da54d893e3b9e72f1df8005900039a
Gurkansson <fil.gustavsson@gmail.com>
2024-05-24 13:49:29 +0200

    divFixSignup-Frida

--- PetBnB/LogIn/LogInView.swift
+++ PetBnB/LogIn/LogInView.swift
@@ -9,115 +9,135 @@ struct LogInView: View {
     var auth = Auth.auth()
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Image("PetHouse")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
-                .padding(.bottom, 40)
-            
-            
-            HStack {
-                
-                Text("Inloggning")
-                    .font(.custom("SF Pro Text", size: 34))
-                    .fontWeight(.semibold)
-                
+        NavigationView {
+            VStack {
                 Spacer()
-            }
-            .padding(.bottom, 40)
-            .padding(.horizontal)
-            
-            
-            VStack(alignment: .leading) {
-                Text("Mejladress: ")
-                    .font(.custom("SF Pro Text", size: 15))
-                    .foregroundColor(.black)
                 
-                TextField("Mejladress", text: $viewModel.email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(.none)
-                    .font(.custom("SF Pro Text", size: 15))
-                
-            }
-            
-            .padding(.horizontal)
-            .padding(.bottom, 20)
-            
-            
-            VStack(alignment: .leading) {
-                Text("Lösenord: ")
-                    .font(.custom("SF Pro Text", size: 15))
-                    .foregroundColor(.black)
+                Image("PetHouse")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .padding(.bottom, 40)
                 
                 
-                SecureField("Lösenord", text: $viewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .font(.custom("SF Pro Text", size: 15))
-            }
-            
-            .padding(.horizontal)
-            .padding(.bottom, 20)
-            
-            Toggle(isOn: $viewModel.rememberMe) {
-                Text("Kom ihåg mig ")
-                    .font(.custom("SF Pro Text", size: 15))
+                HStack {
+                    
+                    Text("Inloggning")
+                        .font(.custom("SF Pro Text", size: 34))
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 40)
+                .padding(.horizontal)
                 
-            }
-            .padding(.horizontal)
-            .padding(.top, 10)
-            
-            HStack {
-                Button(action: {
-                    showSignUpView = true
-                }) {
-                    Text("Registrera")
+                
+                VStack(alignment: .leading) {
+                    Text("Mejladress: ")
                         .font(.custom("SF Pro Text", size: 15))
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .background(Color("primary"))
-                        .foregroundColor(Color("text"))
-                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                    
+                    TextField("Mejladress", text: $viewModel.email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .font(.custom("SF Pro Text", size: 15))
+                    
+                }
+                
+                .padding(.horizontal)
+                .padding(.bottom, 20)
+                
+                
+                VStack(alignment: .leading) {
+                    Text("Lösenord: ")
+                        .font(.custom("SF Pro Text", size: 15))
+                        .foregroundColor(.black)
+                    
+                    
+                    SecureField("Lösenord", text: $viewModel.password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .font(.custom("SF Pro Text", size: 15))
+                }
+                
+                .padding(.horizontal)
+                .padding(.bottom, 20)
+                
+                Toggle(isOn: $viewModel.rememberMe) {
+                    Text("Kom ihåg mig ")
+                        .font(.custom("SF Pro Text", size: 15))
                     
                 }
                 .padding(.horizontal)
+                .padding(.top, 10)
                 
-                Button(action: {
-                    viewModel.signIn()
-                
-                }) {
-                    Text("Logga in")
-                        .font(.custom("SF Pro Text", size: 15))
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .background(Color("primary"))
-                        .foregroundColor(Color("text"))
-                        .cornerRadius(10)
-                }
-                .padding(.horizontal)
-            }
-            .padding(.top, 20)
-            
-            Spacer()
-                .onChange(of: viewModel.signedIn) { newValue in
-                    if newValue {
-                        signedIn = true
+                HStack {
+                    NavigationLink(destination: SignUpView(signedIn: $signedIn).navigationBarBackButtonHidden(true)) {
+                        Text("Registrera")
+                            .font(.custom("SF Pro Text", size: 15))
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .background(Color("primary"))
+                            .foregroundColor(Color("text"))
+                            .cornerRadius(10)
                     }
+                    .padding(.horizontal)
+                    
+                    /*  Button(action: {
+                        showSignUpView = true
+                    }) {
+                       Text("Registrera")
+                            .font(.custom("SF Pro Text", size: 15))
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .background(Color("primary"))
+                            .foregroundColor(Color("text"))
+                            .cornerRadius(10)
+                        
+                    }
+                    .padding(.horizontal)*/
+                    
+                    Button(action: {
+                        viewModel.signIn()
+                        
+                    }) {
+                        Text("Logga in")
+                            .font(.custom("SF Pro Text", size: 15))
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .background(Color("primary"))
+                            .foregroundColor(Color("text"))
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal)
                 }
-                .sheet(isPresented: $showSignUpView) {
-                    SignUpView()
+                .padding(.top, 20)
+                
+                Spacer()
+                    .onChange(of: viewModel.signedIn) { newValue in
+                        if newValue {
+                            signedIn = true
+                        }
+                    }
+                    .sheet(isPresented: $showSignUpView) {
+                        SignUpView(signedIn: $signedIn)
+                    }
+            }.onAppear {
+                if let user = auth.currentUser {
+                    signedIn = false
                 }
-        }.onAppear {
-            if let user = auth.currentUser {
-                signedIn = true
             }
         }
     }
 }
 
 
-#Preview {
-    LogInView(signedIn: .constant(false))
+//#Preview {
+  //  LogInView(signedIn: .constant(false))
+//}
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView(signedIn: .constant(false))
+    }
 }
+
+
--- PetBnB/LogIn/SignUpView.swift
+++ PetBnB/LogIn/SignUpView.swift
@@ -4,85 +4,91 @@ import Foundation
 
 struct SignUpView: View {
     @ObservedObject var viewModel = SignUpViewModel()
-    
+    @Binding var signedIn: Bool
+   
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 20) {
-                Image("catimage")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                
-                Text("Ny användare")
-                    .font(.largeTitle)
-                    .padding(.bottom, 20)
+        VStack(spacing: 20) {
+            Image("PetHouse")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+            
+            Text("Ny användare")
+                .font(.largeTitle)
+                .padding(.bottom, 20)
+            
+            VStack(alignment: .leading) {
+                Text("Namn:")
+                    .padding(.horizontal, 20)
                 
                 TextField("Namn:", text: $viewModel.name)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 20)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Mejladress: ")
                     .padding(.horizontal, 20)
                 
                 TextField("Mejladress:", text: $viewModel.email)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
                     .keyboardType(.emailAddress)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Lösenord: ")
+                    .padding(.horizontal, 20)
                 
                 SecureField("Lösenord:", text: $viewModel.password)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
                     .textContentType(.newPassword)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Bekräfta lösenord: ")
+                    .padding(.horizontal, 20)
                 
                 SecureField("Bekräfta lösenord:", text: $viewModel.confirmPassword)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
                     .textContentType(.newPassword)
-                
-                if !viewModel.errorMessage.isEmpty {
-                    Text(viewModel.errorMessage)
-                        .foregroundColor(.red)
-                        .padding(.horizontal)
+            }
+            
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage)
+                    .foregroundColor(.red)
+                    .padding(.horizontal)
+            }
+            
+            HStack(spacing: 10) {
+                NavigationLink(destination: LogInView(signedIn: $signedIn).navigationBarBackButtonHidden(true)) {
+                    Text("Tillbaka")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("primary"))
+                        .foregroundColor(Color("text"))
+                        .cornerRadius(8)
                 }
                 
-                HStack(spacing: 10) {
-                    Button(action: {
-                        self.viewModel.showLogInView = true
-                    }){
-                        Text("Tillbaka")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color("primary"))
-                            .foregroundColor(Color("text"))
-                            .cornerRadius(8)
-                    }
-                    
-                    Button(action: {
-                        self.viewModel.register()
-                    }) {
-                        Text("Registrera")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color("primary"))
-                            .foregroundColor(Color("text"))
-                            .cornerRadius(8)
-                    }
-                }
-                .padding(.horizontal, 20)
-                
-                NavigationLink(destination: ExploreView().navigationBarHidden(true), isActive: $viewModel.registrationSuccess) {
-                    EmptyView()
+                Button(action: {
+                    self.viewModel.register(signedIn: $signedIn)
+                }) {
+                    Text("Registrera")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("primary"))
+                        .foregroundColor(Color("text"))
+                        .cornerRadius(8)
                 }
             }
-            .sheet(isPresented: $viewModel.showLogInView) {
-                LogInView(signedIn: .constant(false))
+            .padding(.horizontal, 20)
+            
+            NavigationLink(destination: MainTabView().navigationBarBackButtonHidden(true), isActive: $viewModel.registrationSuccess) {
+                EmptyView()
             }
-            .padding()
         }
-        .navigationDestination(isPresented: $viewModel.registrationSuccess) {
-            ExploreView().navigationBarHidden(true)
-        }
+        .padding()
     }
-    
-    
 }
-
-
-
