ae0dbe9ec94467db21a90bd04ecc55802d85eb5c
Ina Burström <inaburstrom@hotmail.com>
2024-05-27 08:59:46 +0200

    Added updateUserHomeIDs function and updated save home with new variables that were added in home model.

--- PetBnB/Utils/FirestoreUtils.swift
+++ PetBnB/Utils/FirestoreUtils.swift
@@ -2,11 +2,12 @@ import Foundation
 import FirebaseStorage
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+import FirebaseAuth
 import Combine
 
 class FirestoreUtils {
     private let db = Firestore.firestore()
-
+    
     func fetchHomes(completion: @escaping (Result<[Home], Error>) -> Void) {
         db.collection("homes").getDocuments { (querySnapshot, error) in
             if let error = error {
@@ -27,7 +28,7 @@ class FirestoreUtils {
             }
         }
     }
-
+    
     func updateFavoriteStatus(homeID: String, isFavorite: Bool, userID: String) -> AnyPublisher<Void, Error> {
         let userFavoritesRef = db.collection("users").document(userID).collection("favorites")
         return Future<Void, Error> { promise in
@@ -51,7 +52,7 @@ class FirestoreUtils {
         }
         .eraseToAnyPublisher()
     }
-
+    
     func fetchFavoriteHomes(userID: String, completion: @escaping (Result<[Home], Error>) -> Void) {
         let userFavoritesRef = db.collection("users").document(userID).collection("favorites")
         userFavoritesRef.getDocuments { (querySnapshot, error) in
@@ -63,7 +64,7 @@ class FirestoreUtils {
                 let dispatchGroup = DispatchGroup()
                 var favoriteHomes = [Home]()
                 var fetchError: Error?
-
+                
                 for homeID in favoriteHomeIDs {
                     dispatchGroup.enter()
                     homesRef.document(homeID).getDocument { (document, error) in
@@ -80,7 +81,7 @@ class FirestoreUtils {
                         dispatchGroup.leave()
                     }
                 }
-
+                
                 dispatchGroup.notify(queue: .main) {
                     if let fetchError = fetchError {
                         completion(.failure(fetchError))
@@ -93,104 +94,140 @@ class FirestoreUtils {
     }
     
     func fetchUser(withID id: String, completion: @escaping (Result<User, Error>) -> Void) {
-            db.collection("users").document(id).getDocument { (document, error) in
-                if let document = document, document.exists {
-                    do {
-                        let user = try document.data(as: User.self)
-                        completion(.success(user))
-                    } catch let error {
-                        completion(.failure(error))
-                    }
-                } else {
-                    if let error = error {
-                        completion(.failure(error))
-                    } else {
-                        completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])))
-                    }
-                }
-            }
-        }
-    
-    func saveHome(homeTitle: String, beds: Int, rooms: Int, size: Int, additionalInfo: String, city: String, availability: Int, selectedImages: [UIImage], animalCount: Int, animalType: [String], animalAge: [Int], animalInfo: [String], rating: Double, completion: @escaping (Result<Void, Error>) -> Void) {
-            let homeRef = db.collection("homes").document()
-
-            var animalInfos: [String: AnimalInfo] = [:]
-            for index in 0..<animalCount {
-                let animalInfo = AnimalInfo(type: animalType[index], age: animalAge[index], additionalInfoAnimal: animalInfo[index])
-                animalInfos["animal\(index)"] = animalInfo
-            }
-
-            uploadImages(images: selectedImages) { result in
-                switch result {
-                case .success(let imageUrls):
-                    let home = Home(
-                        id: homeRef.documentID,
-                        name: homeTitle,
-                        beds: beds,
-                        rooms: rooms,
-                        size: size,
-                        animals: animalInfos,
-                        additionalInfoHome: additionalInfo,
-                        city: city,
-                        availability: availability,
-                        images: imageUrls,
-                        rating: rating
-                    )
-
-                    // Spara hem dokumentet
-                    do {
-                        try homeRef.setData(from: home) { error in
-                            if let error = error {
-                                completion(.failure(error))
-                            } else {
-                                completion(.success(()))
-                            }
-                        }
-                    } catch {
-                        completion(.failure(error))
-                    }
-                case .failure(let error):
+        db.collection("users").document(id).getDocument { (document, error) in
+            if let document = document, document.exists {
+                do {
+                    let user = try document.data(as: User.self)
+                    completion(.success(user))
+                } catch let error {
                     completion(.failure(error))
                 }
-            }
-        }
-
-        func uploadImages(images: [UIImage], completion: @escaping (Result<[String: URL], Error>) -> Void) {
-            let storage = Storage.storage()
-            let storageRef = storage.reference().child("homeImages")
-
-            var imageUrls: [String: URL] = [:]
-            var uploadCount = 0
-
-            for (index, image) in images.enumerated() {
-                let imageRef = storageRef.child(UUID().uuidString + ".jpg")
-                if let imageData = image.jpegData(compressionQuality: 0.8) {
-                    imageRef.putData(imageData, metadata: nil) { metadata, error in
-                        if let error = error {
-                            completion(.failure(error))
-                            return
-                        }
-
-                        imageRef.downloadURL { url, error in
-                            if let error = error {
-                                completion(.failure(error))
-                                return
-                            }
-
-                            if let url = url {
-                                imageUrls["image\(index)"] = url
-                                uploadCount += 1
-
-                                if uploadCount == images.count {
-                                    completion(.success(imageUrls))
-                                }
-                            }
-                        }
-                    }
+            } else {
+                if let error = error {
+                    completion(.failure(error))
                 } else {
-                    completion(.failure(NSError(domain: "imageDataError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Could not convert image to data."])))
-                    return
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])))
                 }
             }
         }
     }
+    
+    func saveHome(homeTitle: String, beds: Int, rooms: Int, size: Int, additionalInfo: String, city: String, availability: Int, selectedImages: [UIImage], animalCount: Int, animalType: [String], animalAge: [Int], animalInfo: [String], rating: Double, completion: @escaping (Result<Void, Error>) -> Void) {
+        
+        guard let userID = Auth.auth().currentUser?.uid else {
+            completion(.failure(NSError(domain: "SaveHome", code: -1, userInfo: [NSLocalizedDescriptionKey: "No user is logged in"])))
+            return
+        }
+        
+        let homeRef = db.collection("homes").document()
+        
+        var animalInfos: [String: AnimalInfo] = [:]
+        for index in 0..<animalCount {
+            let animalInfo = AnimalInfo(type: animalType[index], age: animalAge[index], additionalInfoAnimal: animalInfo[index])
+            animalInfos["animal\(index)"] = animalInfo
+        }
+        
+        uploadImages(images: selectedImages) { result in
+            switch result {
+            case .success(let imageUrls):
+                let home = Home(
+                    id: homeRef.documentID,
+                    userID: userID,
+                    name: homeTitle,
+                    beds: beds,
+                    rooms: rooms,
+                    size: size,
+                    animals: animalInfos,
+                    additionalInfoHome: additionalInfo,
+                    city: city,
+                    availability: availability,
+                    images: imageUrls,
+                    rating: rating
+                )
+                
+                // Save home document
+                do {
+                    try homeRef.setData(from: home) { error in
+                        if let error = error {
+                            completion(.failure(error))
+                        } else {
+                            self.updateUserHomeIDs(userID: userID, homeID: homeRef.documentID, completion: completion)
+                        }
+                    }
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func updateUserHomeIDs(userID: String, homeID: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let userRef = db.collection("users").document(userID)
+        
+        userRef.getDocument { document, error in
+            if let document = document, document.exists {
+                userRef.updateData([
+                    "homeIDs": FieldValue.arrayUnion([homeID])
+                ]) { error in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(()))
+                    }
+                }
+            } else {
+                // If document does not exist, create the document with homeID
+                userRef.setData([
+                    "homeIDs": [homeID]
+                ], merge: true) { error in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(()))
+                    }
+                }
+            }
+        }
+    }
+    
+    func uploadImages(images: [UIImage], completion: @escaping (Result<[String: URL], Error>) -> Void) {
+        let storage = Storage.storage()
+        let storageRef = storage.reference().child("homeImages")
+        
+        var imageUrls: [String: URL] = [:]
+        var uploadCount = 0
+        
+        for (index, image) in images.enumerated() {
+            let imageRef = storageRef.child(UUID().uuidString + ".jpg")
+            if let imageData = image.jpegData(compressionQuality: 0.8) {
+                imageRef.putData(imageData, metadata: nil) { metadata, error in
+                    if let error = error {
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    imageRef.downloadURL { url, error in
+                        if let error = error {
+                            completion(.failure(error))
+                            return
+                        }
+                        
+                        if let url = url {
+                            imageUrls["image\(index)"] = url
+                            uploadCount += 1
+                            
+                            if uploadCount == images.count {
+                                completion(.success(imageUrls))
+                            }
+                        }
+                    }
+                }
+            } else {
+                completion(.failure(NSError(domain: "imageDataError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Could not convert image to data."])))
+                return
+            }
+        }
+    }
+}
