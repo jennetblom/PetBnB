f256ea0f70f147bf74669f6381f7172757139215
Ina Burström <inaburstrom@hotmail.com>
2024-06-04 15:52:02 +0200

    Added func updateUnreadMessagesCount and markConversationAsRead and updated sendmessage func to update unreadmsg count.

--- PetBnB/Chat/ChatWindowViewModel.swift
+++ PetBnB/Chat/ChatWindowViewModel.swift
@@ -16,11 +16,10 @@ class ChatWindowViewModel : ObservableObject {
         self.conversationId = conversationId
         fetchMessages()
         fetchOtherUser()
+        markConversationAsRead()
     }
     
     func fetchMessages() {
-       
-        
         db.collection("conversations").document(conversationId).collection("messages")
             .order(by: "timestamp")
             .addSnapshotListener { querySnapshot, error in
@@ -33,10 +32,12 @@ class ChatWindowViewModel : ObservableObject {
                 }
             }
     }
+    
     func sendMessage(_ content: String) {
         guard let currentUserID = Auth.auth().currentUser?.uid else { return }
+        let receiverID = otherUser?.id ?? ""
         
-        let newMessage = Message(senderID: currentUserID, receiverID: otherUser?.id ?? "", content: content, timestamp: Timestamp(date: Date()))
+        let newMessage = Message(senderID: currentUserID, receiverID: receiverID, content: content, timestamp: Timestamp(date: Date()))
         
         do {
             // Add the new message to the messages collection
@@ -45,6 +46,9 @@ class ChatWindowViewModel : ObservableObject {
                     print("Error sending message: \(error)")
                     return
                 }
+                
+                // Update unreadMessagesCount for the receiver
+                self.updateUnreadMessagesCount(receiverID: receiverID)
                 
                 // Update the lastMessage and timestamp in the conversation document
                 self.db.collection("conversations").document(self.conversationId).updateData([
@@ -60,6 +64,55 @@ class ChatWindowViewModel : ObservableObject {
             print("Error sending message: \(error)")
         }
     }
+    
+    func updateUnreadMessagesCount(receiverID: String) {
+        // Increment unreadMessagesCount for the receiver
+        let conversationRef = db.collection("conversations").document(conversationId)
+        conversationRef.getDocument { documentSnapshot, error in
+            if let document = documentSnapshot, document.exists {
+                var data = document.data() ?? [:]
+                var unreadMessagesCount = data["unreadMessagesCount"] as? [String: Int] ?? [:]
+                
+                // Increment unread count for the receiver
+                unreadMessagesCount[receiverID] = (unreadMessagesCount[receiverID] ?? 0) + 1
+                
+                // Update the document with the new unreadMessagesCount
+                conversationRef.updateData(["unreadMessagesCount": unreadMessagesCount]) { error in
+                    if let error = error {
+                        print("Error updating unreadMessagesCount: \(error)")
+                    }
+                }
+            } else {
+                print("Conversation document does not exist")
+            }
+        }
+    }
+    
+    func markConversationAsRead() {
+        // Get conversation from FB
+        let conversationRef = db.collection("conversations").document(conversationId)
+        conversationRef.getDocument { documentSnapshot, error in
+            if let document = documentSnapshot, document.exists {
+                var data = document.data() ?? [:]
+                var unreadMessagesCount = data["unreadMessagesCount"] as? [String: Int] ?? [:]
+                
+                // Update unreadMessagesCount to 0 for current user
+                if let currentUserID = Auth.auth().currentUser?.uid {
+                    unreadMessagesCount[currentUserID] = 0
+                }
+                
+                // Update conversation with new unreadMessagesCount
+                conversationRef.updateData(["unreadMessagesCount": unreadMessagesCount]) { error in
+                    if let error = error {
+                        print("Error updating unreadMessagesCount: \(error)")
+                    }
+                }
+            } else {
+                print("Conversation document does not exist")
+            }
+        }
+    }
+    
     func fetchOtherUser() {
         db.collection("conversations").document(conversationId).getDocument { documentSnapshot, error in
             if let error = error {
